da4c0784955a50847b2f118313f0b783952324af
Rollin Gilman <rdgilman@my.canyons.edu>
2026-05-01 20:46:17 -0700

    a62 getClosestMed

--- main.hpp
+++ main.hpp
@@ -7,14 +7,27 @@
 using namespace std;
 
 int getRandom(void);
-int getClosestMed(int, int, int);
+
+int getClosestMed(int rdnum1, int rdnum2, int rdnum3);
+
 
 int getRandom(void)
 {
-    // TODO
+    return rand() % 100;
 }
 
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
-    // TODO
+    double mean = (rdnum1 + rdnum2 + rdnum3) / 3.0;
+
+    double d1 = abs(rdnum1 - mean);
+    double d2 = abs(rdnum2 - mean);
+    double d3 = abs(rdnum3 - mean);
+
+    if (d1 <= d2 && d1 <= d3)
+        return rdnum1;
+    else if (d2 <= d1 && d2 <= d3)
+        return rdnum2;
+    else
+        return rdnum3;
 }
